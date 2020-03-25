e975478328f9be2f7d45452d58c1a75452b26f21
JeonJunYeop <galid9619@gmail.com>
2020-03-25 15:36:31 +0900

    add database configuration by JJY

---
+++ src/main/java/com/galid/card_refund/common/config/database/LocalDatabaseConfiguration.java
@@ -0,0 +1,23 @@
+package com.galid.card_refund.common.config.database;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@Profile("local")
+public class LocalDatabaseConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.username("root");
+        builder.password("crew1207");
+        builder.driverClassName("com.mysql.cj.jdbc.Driver");
+        builder.url("jdbc:mysql://localhost:3306/refund?serverTimezone=Asia/Seoul&useSSL=false&characterEncoding=UTF-8");
+        return builder.build();
+    }
+}
