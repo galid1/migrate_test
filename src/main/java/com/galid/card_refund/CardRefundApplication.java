727cf9011ac397efe9b5c8286592412b0dace781
JeonJunYeop <galid9619@gmail.com>
2020-03-25 13:46:29 +0900

    init

---
+++ src/main/java/com/galid/card_refund/CardRefundApplication.java
@@ -0,0 +1,13 @@
+package com.galid.card_refund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardRefundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardRefundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galid/card_refund/CardRefundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galid.card_refund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardRefundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
