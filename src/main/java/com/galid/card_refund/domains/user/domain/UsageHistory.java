4a377b5ae8e6d8a91258bf7fc4664c9454214640
JeonJunYeop <galid9619@gmail.com>
2020-04-02 09:49:11 +0900

    modify UserEntity's UsageHistory by JJY - UsageHistory 값 객체안의 중복되는 Money로 인해 발생하는 컬럼 이름 중복에러 해결(@AttributeOverride)

--- src/main/java/com/galid/card_refund/domains/user/domain/UsageHistory.java
+++ src/main/java/com/galid/card_refund/domains/user/domain/UsageHistory.java
@@ -3,7 +3,10 @@ package com.galid.card_refund.domains.user.domain;
 import com.galid.card_refund.common.model.Money;
 import lombok.*;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
 import java.time.LocalDate;
 
 @Embeddable
@@ -14,5 +17,10 @@ import java.time.LocalDate;
 public class UsageHistory {
     private LocalDate date;
     private String place;
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "payment_amount"))
     private Money paymentAmount;
+    @AttributeOverride(name = "value", column = @Column(name = "remain_amount"))
+    @Embedded
+    private Money remainAmount;
 }
