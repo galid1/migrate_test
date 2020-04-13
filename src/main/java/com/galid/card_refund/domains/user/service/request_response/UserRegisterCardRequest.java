1e20e8f87db7a1cc8ee449495eb3157dde4ea406
JeonJunYeop <galid9619@gmail.com>
2020-04-13 11:46:50 +0900

    add contraint to UserRegisterCardRequest Dto by JJY

--- src/main/java/com/galid/card_refund/domains/user/service/request_response/UserRegisterCardRequest.java
+++ src/main/java/com/galid/card_refund/domains/user/service/request_response/UserRegisterCardRequest.java
@@ -5,10 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class UserRegisterCardRequest {
+    @NotEmpty
     private String cardNum;
+    @NotEmpty
     private String serial;
 }
