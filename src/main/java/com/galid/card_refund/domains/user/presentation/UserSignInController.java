43fc2a18be0c6c7ac35bd252f798d5373256de78
JeonJunYeop <galid9619@gmail.com>
2020-04-14 10:47:16 +0900

    hotfix UserSignInController (add @PostMapping) by JJY

--- src/main/java/com/galid/card_refund/domains/user/presentation/UserSignInController.java
+++ src/main/java/com/galid/card_refund/domains/user/presentation/UserSignInController.java
@@ -4,6 +4,7 @@ import com.galid.card_refund.domains.user.service.UserSignInService;
 import com.galid.card_refund.domains.user.service.request_response.UserSignInRequest;
 import com.galid.card_refund.domains.user.service.request_response.UserSignInResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import javax.validation.Valid;
 public class UserSignInController {
     private final UserSignInService userSignInService;
 
+    @PostMapping("/users/sign-in")
     public UserSignInResponse signIn(@RequestBody @Valid UserSignInRequest userSignInRequest) {
         return userSignInService.signIn(userSignInRequest);
     }
