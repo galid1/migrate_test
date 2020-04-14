6c98de4bc3405e72f72d61f23032cac238af6013
JeonJunYeop <galid9619@gmail.com>
2020-04-14 17:33:21 +0900

    hotfix signUp API by JJY - 기본생성자가 존재하지 않는 이유로 Request 생성이 안되는 문제 해결

--- src/main/java/com/galid/card_refund/domains/user/service/request_response/UserSignUpRequest.java
+++ src/main/java/com/galid/card_refund/domains/user/service/request_response/UserSignUpRequest.java
@@ -3,10 +3,12 @@ package com.galid.card_refund.domains.user.service.request_response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Getter
 public class UserSignUpRequest {
