86cfeb94e094d1b79553b1664668ae3fbabae8d9
JeonJunYeop <galid9619@gmail.com>
2020-04-22 15:19:29 +0900

    hotfix record usage history by JJY - 카드 사용시 userId를 파라미터로 넘겨줄수가 없었음, 따라서 message에 담긴 card num을 이용해 기록하도록 변경

--- src/main/java/com/galid/card_refund/domains/user/service/UserCardUsageHistoryService.java
+++ src/main/java/com/galid/card_refund/domains/user/service/UserCardUsageHistoryService.java
@@ -1,13 +1,17 @@
 package com.galid.card_refund.domains.user.service;
 
+import com.galid.card_refund.common.model.Money;
 import com.galid.card_refund.domains.user.domain.UsageHistory;
 import com.galid.card_refund.domains.user.domain.UserEntity;
 import com.galid.card_refund.domains.user.domain.UserRepository;
 import com.galid.card_refund.domains.user.service.request_response.UsageHistoryResponse;
+import com.galid.card_refund.domains.user.service.request_response.UsageRecordRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,10 +41,63 @@ public class UserCardUsageHistoryService {
     }
 
     @Transactional
-    public void recordUsageHistory(Long userId, UsageHistory usageHistory) {
+    public void recordUsageHistory(UsageRecordRequest request) {
+        String[] payMessageLines = request.getMessage()
+                .split("\n\n")[0]
+                .split("\n");
+
+        Long userId = getUserIdByCardNum(payMessageLines[1]
+                .substring(payMessageLines[1].length() - 4));
+
+        UsageHistory usageHistory = toUsageHistory(payMessageLines[2], payMessageLines[3], payMessageLines[4]);
+
         UserEntity userEntity = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
 
         userEntity.recordCardUsage(usageHistory);
+    }
+
+    private Long getUserIdByCardNum(String lastFourDigitOfCard) {
+        return userRepository.findByLastFourDigitOfCardNum(lastFourDigitOfCard)
+                .orElseThrow(() -> new IllegalArgumentException("해당 Card를 등록한 User가 존재하지 않습니다."))
+                .getUserId();
+    }
+
+    private UsageHistory toUsageHistory(String dateAndPlaceLine, String paymentLine, String remainAmountLine) {
+        String[] dateAndPlaceTemp = dateAndPlaceLine.split(" ");
+
+        LocalDateTime date = toLocalDateTime(dateAndPlaceTemp[0].split("/"), dateAndPlaceTemp[1].split(":"));
+        String place = dateAndPlaceTemp[2];
+        double paymentAmount = Double.parseDouble(removeLastChar(paymentLine));
+        double remainAmount = formatRemainAmount(remainAmountLine);
+
+        return UsageHistory.builder()
+                .date(date)
+                .place(place)
+                .paymentAmount(new Money(paymentAmount))
+                .remainAmount(new Money(remainAmount))
+                .build();
+    }
+
+    private String removeLastChar(String str) {
+        return str.substring(0, str.length() - 1);
+    }
+
+    private LocalDateTime toLocalDateTime(String[] monthAndDate, String[] hourAndMin) {
+        return LocalDateTime.of(LocalDateTime.now().getYear(),
+                Integer.parseInt(monthAndDate[0]),
+                Integer.parseInt(monthAndDate[1]),
+                Integer.parseInt(hourAndMin[0]),
+                Integer.parseInt(hourAndMin[1]));
+    }
+
+    private double formatRemainAmount(String remainAmountLine) {
+        String amountWonWithRest = remainAmountLine.split(":")[1];
+        String amountWithRest = removeLastChar(amountWonWithRest);
+        double amount = Double.parseDouble(Arrays.asList(amountWithRest.split(","))
+                .stream()
+                .reduce("", (init, nextLine) -> init + nextLine));
+
+        return amount;
     }
 }
