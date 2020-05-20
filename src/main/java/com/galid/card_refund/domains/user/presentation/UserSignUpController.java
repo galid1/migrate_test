a6e4e67a6cac538d0dcb6196e9cfb8d0d5c33fc2
JeonJunYeop <galid9619@gmail.com>
2020-05-20 12:02:06 +0900

    modify UserSignUpController by JJY - 사용하지 않는 import  제거

--- src/main/java/com/galid/card_refund/domains/user/presentation/UserSignUpController.java
+++ src/main/java/com/galid/card_refund/domains/user/presentation/UserSignUpController.java
@@ -6,7 +6,6 @@ import com.galid.card_refund.domains.user.service.request_response.UserSignUpRes
 import lombok.*;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
