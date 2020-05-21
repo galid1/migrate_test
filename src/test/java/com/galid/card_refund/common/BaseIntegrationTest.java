39e49d626caec02903167496c8d2c96cc0443c2f
JeonJunYeop <galid9619@gmail.com>
2020-05-21 10:55:37 +0900

    modify BaseIntegrationTest by JJY - active profile 추가

--- src/test/java/com/galid/card_refund/common/BaseIntegrationTest.java
+++ src/test/java/com/galid/card_refund/common/BaseIntegrationTest.java
@@ -1,17 +1,21 @@
 package com.galid.card_refund.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 @Transactional
-@Ignore             // 이 클래스의 테스트를 건너띔 (상속전용이므로)
+@Disabled             // 이 클래스의 테스트를 건너띔 (상속전용이므로)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class BaseIntegrationTest {
     @Autowired
     protected MockMvc mvc;
