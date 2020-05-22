fea8ce9df50b2d8767c3d9121f6c86b257afeeb1
JeonJunYeop <galid9619@gmail.com>
2020-05-22 15:57:30 +0900

    add getUserPassportImage API by JJY - 유저의 여권 이미지를 가져오는 API 추가 (여권 이미지 평가가 완료되지 않은 경우 에러)

--- src/main/java/com/galid/card_refund/domains/user/presentation/UserController.java
+++ src/main/java/com/galid/card_refund/domains/user/presentation/UserController.java
@@ -2,25 +2,33 @@ package com.galid.card_refund.domains.user.presentation;
 
 import com.galid.card_refund.domains.user.service.UserInformationService;
 import com.galid.card_refund.domains.user.service.request_response.UserInformationResponse;
+import com.galid.card_refund.domains.user.service.request_response.UserPassportImageResponse;
 import com.galid.card_refund.domains.user.service.request_response.UserPassportStatusResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/users/{userId}")
 public class UserController {
     private final UserInformationService service;
 
-    @GetMapping("/users/{userId}/information")
+    @GetMapping("/information")
     public UserInformationResponse getUserInformation(@PathVariable("userId") Long userId) {
         return service.getUserInformation(userId);
     }
 
-    @GetMapping("/users/{userId}/passport-status")
+    @GetMapping("/passport-status")
     public UserPassportStatusResponse getUserPassportStatus(@PathVariable("userId") Long userId) {
         return service.getUserPassportStatus(userId);
+    }
+
+    @GetMapping("/passport-image")
+    public UserPassportImageResponse getPassportImage(@PathVariable("userId")Long userId) {
+        return service.getUserPassportImage(userId);
     }
 
 }
--- src/main/java/com/galid/card_refund/domains/user/service/UserInformationService.java
+++ src/main/java/com/galid/card_refund/domains/user/service/UserInformationService.java
@@ -3,6 +3,7 @@ package com.galid.card_refund.domains.user.service;
 import com.galid.card_refund.domains.user.domain.UserEntity;
 import com.galid.card_refund.domains.user.domain.UserRepository;
 import com.galid.card_refund.domains.user.service.request_response.UserInformationResponse;
+import com.galid.card_refund.domains.user.service.request_response.UserPassportImageResponse;
 import com.galid.card_refund.domains.user.service.request_response.UserPassportStatusResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -29,5 +30,12 @@ public class UserInformationService {
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
 
         return new UserPassportStatusResponse(userEntity.getPassportStatus());
+    }
+
+    public UserPassportImageResponse getUserPassportImage(Long userId) {
+        UserEntity userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+
+        return new UserPassportImageResponse(userEntity.getPassPortImagePath());
     }
 }
