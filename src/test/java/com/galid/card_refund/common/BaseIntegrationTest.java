b4ab49b8a995eb0ebfadeffa022bc921ac965731
JeonJunYeop <galid9619@gmail.com>
2020-05-28 14:24:58 +0900

    modify BaseIntegrationTest by JJY - scheme 변경 (http -> https)

--- src/test/java/com/galid/card_refund/common/BaseIntegrationTest.java
+++ src/test/java/com/galid/card_refund/common/BaseIntegrationTest.java
@@ -15,7 +15,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Disabled             // 이 클래스의 테스트를 건너띔 (상속전용이므로)
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-@AutoConfigureRestDocs(outputDir = "target/snippets", uriHost = "rest.tour-cash.com", uriPort = 443)
+@AutoConfigureRestDocs(outputDir = "target/snippets",
+                       uriScheme = "https",
+                       uriHost = "rest.tour-cash.com",
+                       uriPort = 443)
 public class BaseIntegrationTest {
     @Autowired
     protected MockMvc mvc;
