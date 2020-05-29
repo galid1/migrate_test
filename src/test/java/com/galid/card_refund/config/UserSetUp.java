6951007541989add1d319558ea47cb6c0f05407c
JeonJunYeop <galid9619@gmail.com>
2020-05-29 17:26:31 +0900

    add UserSetUp by JJY - 통합 테스트를 위한 디비 더미데이터 저장 도우미 클래스 추가

---
+++ src/test/java/com/galid/card_refund/config/UserSetUp.java
@@ -0,0 +1,22 @@
+package com.galid.card_refund.config;
+
+import com.galid.card_refund.domains.user.domain.UserEntity;
+import com.galid.card_refund.domains.user.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserSetUp {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Long saveUser(String testDeviceId, String testNickname, String testPassportImageUrl) {
+        UserEntity savedUser = userRepository.save(UserEntity.builder()
+                .deviceId(testDeviceId)
+                .nickname(testNickname)
+                .build());
+        savedUser.uploadPassportImagePath(testPassportImageUrl);
+
+        return savedUser.getUserId();
+    }
+}
