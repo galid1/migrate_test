68c7b4c473ccf4602cfd8ab3de020e700bce3e2e
JeonJunYeop <galid9619@gmail.com>
2020-05-29 17:27:04 +0900

    add UserSignInControllerTest by JJY - 로그인 테스트 코드 추가

---
+++ src/test/java/com/galid/card_refund/domains/user/presentation/UserSignInControllerTest.java
@@ -0,0 +1,50 @@
+package com.galid.card_refund.domains.user.presentation;
+
+import com.galid.card_refund.common.BaseIntegrationTest;
+import com.galid.card_refund.config.UserSetUp;
+import com.galid.card_refund.domains.user.service.request_response.UserSignInRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class UserSignInControllerTest extends BaseIntegrationTest {
+    @Autowired
+    private UserSetUp userSetUp;
+
+    @Test
+    public void 로그인() throws Exception {
+        //given
+        String DEVICE_ID = "TEST";
+        Long userId = userSetUp.saveUser(DEVICE_ID, "TEST", "TEST");
+        UserSignInRequest signInRequest = new UserSignInRequest(DEVICE_ID);
+
+        //when
+        ResultActions resultActions = mvc.perform(post("/users/sign-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signInRequest))
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(document("user/{method-name}",
+                        requestFields(
+                                fieldWithPath("deviceId").description("디바이스 고유 id")
+                        ),
+                        responseFields(
+                                fieldWithPath("userId").description("Database상의 user_id")
+                        )
+                ));
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("userId", is(notNullValue())));
+    }
+}
