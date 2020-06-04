c72697d02c1f995f081da835030848289e2a4d21
JeonJunYeop <galid9619@gmail.com>
2020-06-04 16:34:46 +0900

    refactoring CardRepository by JJY  - 사용하지 않는 코드 제거

--- src/main/java/com/galid/card_refund/domains/card/card/domain/CardRepository.java
+++ src/main/java/com/galid/card_refund/domains/card/card/domain/CardRepository.java
@@ -6,6 +6,4 @@ import java.util.Optional;
 
 public interface CardRepository extends JpaRepository<CardEntity, Long> {
     Optional<CardEntity> findByCardInformation_CardNum(String cardNum);
-
-    Optional<CardEntity> findByOwnerId(Long ownerId);
 }
