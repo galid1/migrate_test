4656d11eb656a5a995a807698a94905bbe7525a9
JeonJunYeop <galid9619@gmail.com>
2020-06-10 16:00:00 +0900

    modify UserControllerTest by JJY - header에 token 추가

--- src/test/java/com/galid/card_refund/domains/user/presentation/UserControllerTest.java
+++ src/test/java/com/galid/card_refund/domains/user/presentation/UserControllerTest.java
@@ -4,11 +4,15 @@ import com.galid.card_refund.common.BaseIntegrationTest;
 import com.galid.card_refund.config.UserSetUp;
 import com.galid.card_refund.domains.user.domain.UserEntity;
 import com.galid.card_refund.domains.user.domain.UserPassportStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
@@ -23,28 +27,42 @@ public class UserControllerTest extends BaseIntegrationTest {
     @Autowired
     private UserSetUp userSetUp;
 
+    private UserEntity TEST_USER_ENTITY;
+    private String TEST_TOKEN;
+
+    @BeforeEach
+    public void init() {
+        TEST_USER_ENTITY = userSetUp.saveUser();
+        TEST_TOKEN = userSetUp.signIn();
+    }
+
     @Test
     public void 유저정보_조회() throws Exception {
         //given
-        UserEntity userEntity = userSetUp.saveUser();
 
         //when
-        ResultActions resultActions = mvc.perform(get("/users/{userId}/information", userEntity.getUserId())
-                .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
+        ResultActions resultActions = mvc.perform(get("/users/{userId}/information", TEST_USER_ENTITY.getUserId())
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + TEST_TOKEN));
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("nickname").value(TEST_USER_ENTITY.getNickname()))
+                .andExpect(jsonPath("passportImageUrl").value(TEST_USER_ENTITY.getPassPortImagePath()));
+
+        //restdocs
+        resultActions
                 .andDo(document("user/{method-name}",
                         preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer <TOKEN>")
+                        ),
                         responseFields(
                                 fieldWithPath("nickname").description("사용자 닉네임"),
                                 fieldWithPath("passportImageUrl").description("사용자 여권 이미지 경로")
                         )
                 ));
-
-        //then
-        resultActions
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("nickname").value(userEntity.getNickname()))
-                .andExpect(jsonPath("passportImageUrl").value(userEntity.getPassPortImagePath()));
     }
     
 //    @Test
@@ -74,25 +92,27 @@ public class UserControllerTest extends BaseIntegrationTest {
 
     @Test
     public void 여권상태_조회() throws Exception {
-        //given
-        UserEntity userEntity = userSetUp.saveUser();
-
         //when
-        ResultActions resultActions = mvc.perform(get("/users/{userId}/passport-status", userEntity.getUserId())
-                .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andDo(document("user/{method-name}",
-                    preprocessResponse(prettyPrint()),
-                    responseFields(
-                        fieldWithPath("passportStatus").description("여권 상태 정보")
-                    )
-                ));
+        ResultActions resultActions = mvc.perform(get("/users/{userId}/passport-status", TEST_USER_ENTITY.getUserId())
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + TEST_TOKEN));
 
         //then
         resultActions
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("passportStatus").value(UserPassportStatus.ESTIMATING_STATUS.toString()));
-    }
 
+        //restdocs
+        resultActions
+                .andDo(document("user/{method-name}",
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer <TOKEN>")
+                        ),
+                        responseFields(
+                                fieldWithPath("passportStatus").description("여권 상태 정보")
+                        )
+                ));
+    }
 
 }
