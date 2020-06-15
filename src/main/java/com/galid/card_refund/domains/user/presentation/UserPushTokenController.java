53a6aaa31f21949956729340185d958ab2e97b52
JeonJunYeop <galid1@naver.com>
2020-06-15 11:17:33 +0900

    modify PushTokenController by JJY - 중복 request url 코드  리팩토링 (Class 레벨에 @RequestMapping() 추가)

--- src/main/java/com/galid/card_refund/domains/user/presentation/UserPushTokenController.java
+++ src/main/java/com/galid/card_refund/domains/user/presentation/UserPushTokenController.java
@@ -4,21 +4,21 @@ import com.galid.card_refund.domains.user.service.UserPushTokenService;
 import com.galid.card_refund.domains.user.service.request_response.StorePushTokenRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/users/{userId}/push-token")
 public class UserPushTokenController {
     private final UserPushTokenService pushTokenService;
 
-    @PostMapping("/users/{userId}/push-token")
+    @PostMapping
     public ResponseEntity storePushToken(@PathVariable("userId") Long userId,
                                          @RequestBody StorePushTokenRequest request) {
         pushTokenService.storeUserPushToken(userId, request);
 
         return ResponseEntity.ok().build();
     }
+    
+
 }
