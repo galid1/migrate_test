7a7890706fc57e7f8b691e18ca989d650c90ec9b
JeonJunYeop <galid1@naver.com>
2020-06-26 16:28:03 +0900

    hotfix CardEntityTest by JJY - 통합테스트시 중복된 회원가입 오류 해결 ( CardEntityTest 에서, user를 생성하고 Rollback이 되지 않아 발생)

--- src/test/java/com/galid/card_refund/domains/card/domain/CardEntityTest.java
+++ src/test/java/com/galid/card_refund/domains/card/domain/CardEntityTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class CardEntityTest {
     @Autowired
     private CardRepository cardRepository;
