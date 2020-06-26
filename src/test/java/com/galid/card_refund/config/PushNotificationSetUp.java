62e1ffccbf959b1da310bbfb8cc42b6311e02006
JeonJunYeop <galid1@naver.com>
2020-06-26 15:39:47 +0900

    add PushNotificationSetUp by JJY - Push 알림 목처리를 위한 setup 클래스 생성

---
+++ src/test/java/com/galid/card_refund/config/PushNotificationSetUp.java
@@ -0,0 +1,22 @@
+package com.galid.card_refund.config;
+
+import com.galid.card_refund.common.pushnotification.FirebaseCloudMessageService;
+import com.galid.card_refund.common.pushnotification.PushNotificationEvent;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+
+@Component
+public class PushNotificationSetUp {
+    @MockBean
+    private FirebaseCloudMessageService service;
+
+    @PostConstruct
+    public void init() {
+        doNothing().when(service).sendMessageTo(any(PushNotificationEvent.class));
+    }
+}
