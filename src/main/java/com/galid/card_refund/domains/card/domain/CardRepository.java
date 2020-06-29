37eded8e6b6cedcb86c4b75ea379eacdca1d66ff
JeonJunYeop <galid1@naver.com>
2020-06-29 10:17:22 +0900

    modify CardRepository by JJY - 카드 repository에서 userId를 이용해 카드들을 가져오며, 가장 마지막에 등록된 카드를 반환 (UserEntity에 Card가 제거됨으로써, User를 통해 가져오던 Card 기능대신 CardRepository에서 가져도오도록 변경)

--- src/main/java/com/galid/card_refund/domains/card/domain/CardRepository.java
+++ src/main/java/com/galid/card_refund/domains/card/domain/CardRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface CardRepository extends JpaRepository<CardEntity, Long> {
     Optional<CardEntity> findByCardInformation_CardNum(String cardNum);
+    Optional<CardEntity> findFirstByOwnerIdOrderByCreatedDateDesc(Long ownerId);
 }
