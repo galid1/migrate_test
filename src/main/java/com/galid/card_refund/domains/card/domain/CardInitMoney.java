cca20eaab2021e21fad4f7d688dd9df7eade1e09
JeonJunYeop <galid1@naver.com>
2020-06-30 09:56:34 +0900

    modify CardInitMoney by JJY - 초기금액 추가 (40)

--- src/main/java/com/galid/card_refund/domains/card/domain/CardInitMoney.java
+++ src/main/java/com/galid/card_refund/domains/card/domain/CardInitMoney.java
@@ -3,7 +3,7 @@ package com.galid.card_refund.domains.card.domain;
 import com.galid.card_refund.common.model.Money;
 
 public enum CardInitMoney {
-    TEN(100000), TWENTY(200000), THIRTY(300000), FIFTY(500000);
+    TEN(100000), TWENTY(200000), THIRTY(300000), FORTY(400000), FIFTY(500000);
 
     Money money;
 
