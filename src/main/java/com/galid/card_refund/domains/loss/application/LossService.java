15d70a43103deacc4c19f9deff3fe75655da6476
peoplusapply <peoplusapply@gmail.com>
2020-07-08 13:56:59 +0900

    modify LossService, LossRepository by JJY - 분실신고 엔티티 조회 조건 변경 (분실 취소가 가능하기 때문에, cardId로만 조회하는 경우 여러개의 분실신고 건이 발견됨)

--- src/main/java/com/galid/card_refund/domains/loss/application/LossService.java
+++ src/main/java/com/galid/card_refund/domains/loss/application/LossService.java
@@ -4,8 +4,11 @@ import com.galid.card_refund.domains.card.domain.CardEntity;
 import com.galid.card_refund.domains.card.domain.CardRepository;
 import com.galid.card_refund.domains.loss.domain.LossEntity;
 import com.galid.card_refund.domains.loss.domain.LossRepository;
+import com.galid.card_refund.domains.loss.domain.LossStatus;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,11 +18,12 @@ public class LossService {
     private final LossRepository lossRepository;
     private final CardRepository cardRepository;
 
+    @Transactional
     public Long reportLossCard(Long ownerId) {
         CardEntity card = cardRepository.findFirstByOwnerIdOrderByCreatedDateDesc(ownerId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 유저가 소유한 카드가 존재하지 않습니다."));
 
-        validateDuplicatedLossReport(card);
+        validateDuplicatedLossReport(card.getCardId());
         card.reportLoss();
         return saveLossEntity(card.getCardId()).getLossId();
     }
@@ -32,10 +36,13 @@ public class LossService {
         return savedLossEntity;
     }
 
-    private void validateDuplicatedLossReport(CardEntity lossCard) {
-        Optional<LossEntity> lossEntity = lossRepository.findById(lossCard.getCardId());
+    private void validateDuplicatedLossReport(Long cardId) {
+        Optional<LossEntity> lossEntity = getCurrentReceiptLossEntity(cardId);
         if (lossEntity.isPresent())
             throw new IllegalStateException("이미 분실 요청된 카드입니다.");
     }
 
+    private Optional<LossEntity> getCurrentReceiptLossEntity(Long cardId) {
+        return lossRepository.findFirstByCardIdAndLossStatus(cardId, LossStatus.RECEIPT_STATUS);
+    }
 }
