9c443e3fdaa75c85c6d11f3295083528501b1a70
galid1 <galid1@naver.com>
2020-07-08 16:43:46 +0900

    modify LossEntity by JJY - 분실신고 처리(카드사를 통해 해결한 후 상태 변경 메소드) 메소드 추가 - 분실신고 취소 메소드 추가

--- src/main/java/com/galid/card_refund/domains/loss/domain/LossEntity.java
+++ src/main/java/com/galid/card_refund/domains/loss/domain/LossEntity.java
@@ -20,7 +20,6 @@ public class LossEntity extends BaseAuditEntity {
     @Enumerated(value = EnumType.STRING)
     private LossStatus lossStatus;
 
-
     @Builder
     public LossEntity(Long lossCardId) {
         this.lossCardId = lossCardId;
@@ -28,17 +27,14 @@ public class LossEntity extends BaseAuditEntity {
     }
 
     public void processLoss() {
-        validateLossStatus();
+        if(this.lossStatus != LossStatus.RECEIPT_STATUS)
+            throw new IllegalStateException("이미 분실처리가 완료되었거나 취소된 상태입니다.");
         this.lossStatus = LossStatus.COMPLETE_STATUS;
     }
 
     public void cancel() {
-        validateLossStatus();
+        if(this.lossStatus == LossStatus.COMPLETE_STATUS)
+            throw new IllegalStateException("이미 분실 처리가 완료된 경우 취소가 불가능합니다.");
         this.lossStatus = LossStatus.CANCEL_STATUS;
-    }
-
-    private void validateLossStatus() {
-        if(this.lossStatus != LossStatus.RECEIPT_STATUS)
-            throw new IllegalStateException("이미 분실처리가 완료 되었거나, 취소된 분실 요청입니다.");
     }
 }
