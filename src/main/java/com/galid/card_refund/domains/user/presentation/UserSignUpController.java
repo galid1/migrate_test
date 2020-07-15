f151c7c0160636e4aefa796a837da6967d09caa7
전준엽 <galid1@naver.com>
2020-07-16 08:26:51 +0900

    modify
    - 사용하지 않는 import 제거

--- src/main/java/com/galid/card_refund/domains/user/presentation/UserSignUpController.java
+++ src/main/java/com/galid/card_refund/domains/user/presentation/UserSignUpController.java
@@ -3,13 +3,10 @@ package com.galid.card_refund.domains.user.presentation;
 import com.galid.card_refund.domains.user.application.UserSignUpService;
 import com.galid.card_refund.domains.user.application.request_response.UserSignUpRequest;
 import com.galid.card_refund.domains.user.application.request_response.UserSignUpResponse;
-import lombok.*;
-import org.springframework.http.MediaType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
